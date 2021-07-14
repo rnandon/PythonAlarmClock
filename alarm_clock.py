3ac7bdb9620fcfb2b3fc539eb9733968df437842
Ryan <ryan@ryannd.com>
2021-07-14 11:33:32 -0500

    Implemented the main function to create a new alarm and set the time until the alarm goes off

--- alarm_clock.py
+++ alarm_clock.py
@@ -10,23 +10,25 @@ class Alarm_Clock:
         # Format the current time
         current_time = time.localtime()
         hours_and_minutes = [current_time[3], current_time[4]]
+
+        print(f'Current time is {hours_and_minutes[0]}:{hours_and_minutes[1]}')
         
         self.current_time = hours_and_minutes
 
         if self.alarm_status == "on" and self.current_time[0] == self.alarm_time[0] and self.current_time[1] == self.alarm_time[1]:
             self.sound_alarm()
 
-    def toggle_alarm(self):
-        if self.alarm_status == "off":
-            self.alarm_status == "on"
-        else:
-            self.alarm_status = "off"
+    def toggle_alarm(self, status):
+        self.alarm_status = status
 
     def set_alarm_time(self, time):
         # Alarm time should be passed in using 24 hour format
         # I.E. "06:00", "23:15"
         hms = time.split(":")
-        self.alarm_time = hms
+        self.alarm_time = [int(value) for value in hms]
+        print(f'Alarm is set for {time}')
+        self.toggle_alarm('on')
 
     def sound_alarm(self):
         print("Your alarm is going off!!!")
+        self.toggle_alarm('off')
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from alarm_clock import Alarm_Clock
+import time
 
 def main():
-    pass
+    alarm = Alarm_Clock()
+    new_alarm_time = time.localtime()
+    new_alarm_time = f'{new_alarm_time[3]}:{new_alarm_time[4] + 1}'
+    alarm.set_alarm_time(new_alarm_time)
+
+    for i in range(100):
+        sleep_time = 5
+        time.sleep(sleep_time)
+        alarm.set_current_time()
+        if alarm.alarm_status == 'off':
+            break
 
 main()
