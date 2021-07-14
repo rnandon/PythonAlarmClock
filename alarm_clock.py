db40380bbb8ef1751901f4eff5fe12eda4ee906b
Ryan <ryan@ryannd.com>
2021-07-14 09:59:59 -0500

    Scaffolded alarm clock class and main function

---
+++ alarm_clock.py
@@ -0,0 +1,14 @@
+class Alarm_Clock:
+    def __init__(self):
+        pass
+
+    def set_current_time(self):
+        pass
+
+    def toggle_alarm(self):
+        pass
+
+    def set_alarm_time(self, time):
+        pass
+
+    
