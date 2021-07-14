badd82ed7ecbcf9fa4e6f56808de133c0efa416a
Ryan <ryan@ryannd.com>
2021-07-14 11:14:34 -0500

    Implemented alarm clock class methods

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,14 +1,32 @@
+import time
+
 class Alarm_Clock:
-    def __init__(self):
-        pass
+    def __init__(self, alarm_status="off", alarm_time="06:00"):
+        self.alarm_status = alarm_status
+        self.alarm_time = alarm_time.split(":")
+        self.current_time = self.set_current_time()
 
     def set_current_time(self):
-        pass
+        # Format the current time
+        current_time = time.localtime()
+        hours_and_minutes = [current_time[3], current_time[4]]
+        
+        self.current_time = hours_and_minutes
+
+        if self.alarm_status == "on" and self.current_time[0] == self.alarm_time[0] and self.current_time[1] == self.alarm_time[1]:
+            self.sound_alarm()
 
     def toggle_alarm(self):
-        pass
+        if self.alarm_status == "off":
+            self.alarm_status == "on"
+        else:
+            self.alarm_status = "off"
 
     def set_alarm_time(self, time):
-        pass
+        # Alarm time should be passed in using 24 hour format
+        # I.E. "06:00", "23:15"
+        hms = time.split(":")
+        self.alarm_time = hms
 
-    
+    def sound_alarm(self):
+        print("Your alarm is going off!!!")
---
+++ main.py
@@ -0,0 +1,6 @@
+from alarm_clock import Alarm_Clock
+
+def main():
+    pass
+
+main()
